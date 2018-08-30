eacbe1f6b46aaee83169a941221340b8e4cab6cd
Katrina Tran <kat.c.tran@gmail.com>
2018-08-29 20:08:32 -0600

    added items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :list
-  validates: :code, length: {is: 4}
+  validates :code, length: {is: 4}
 
 def self.by_code
-  order(:code) asc
+  order(:code)
 end
 
+def item_price
+  "#{self.name} {self.price}"
+end
 
 end
