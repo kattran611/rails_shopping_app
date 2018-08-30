88d6e5b286a1ba8b93e3a0d1b3511c928bbf954b
Katrina Tran <kat.c.tran@gmail.com>
2018-08-29 19:29:35 -0600

    added validations and method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :list
+  validates: :code, length: {is: 4}
+
+def self.by_code
+  order(:code) asc
+end
+
+
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
   belongs_to :store
   has_many :items, dependent: :destroy
+  validates :name, presence: true
 end
